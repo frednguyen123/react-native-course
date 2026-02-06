d32c027bbdc78dcbae7f3ca70d6e47e1e768d6d8
frednguyen123 <fred.nguyen.2000@gmail.com>
2026-02-06 02:25:45 -0800

    Section 7 Redux Favorites - Completed

---
+++ 07x1-redux/screens/FavoritesScreen.js
@@ -0,0 +1,51 @@
+import { View, Text, StyleSheet } from "react-native";
+import { useSelector } from 'react-redux';
+// import { useContext } from "react";
+
+import MealsList from "../components/MealsList/MealsList";
+// import { FavoritesContext } from "../store/context/favorites-context";
+import { MEALS } from "../data/dummy-data";
+
+function FavoritesScreen() {
+  // const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
+
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoriteMealIds.includes(meal.id),
+  );
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meals yet.</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.backgroundColor}>
+      <MealsList items={favoriteMeals} />
+    </View>
+  );
+}
+
+export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#3f2f25",
+    // need to add style on new version of rnative
+  },
+  backgroundColor: {
+    flex: 1, 
+    backgroundColor: "#3f2f25",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
